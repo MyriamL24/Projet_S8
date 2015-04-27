69194d29565d19db9178660b01970ed78af9c69c
Myriam Lopez <mlopez001006@emi.u-bordeaux1.fr>
2015-04-27 16:20:31 +0200

    Modification pour demande d'identifiants

--- Base.py
+++ Base.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 import psycopg2
+import base64
 
-username = ''
-pswd = ''
+#username = ''
+#pswd = base64.b64encode('')
 
-def connexion(Req):
+
+def connexion(Req, username, pswd):
     
     try :
-            conn = psycopg2.connect(dbname =  username, host = "dbserver", user = username,  password = pswd)
+            conn = psycopg2.connect(dbname =  username, host = "dbserver", user = username,  password = base64.b64decode(pswd))
     except:
             print "Impossible de se connecter à la base de données" 
 
@@ -25,4 +27,5 @@ def connexion(Req):
     cur.close()
 
     return data, column_name
+
 
