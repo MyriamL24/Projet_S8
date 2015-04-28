7f1bdaa68774a1ca12d1c55edd36f660c16cb07c
Myriam Lopez <mlopez001006@emi.u-bordeaux1.fr>
2015-04-28 10:28:38 +0200

    Modification du fichier de parsage

--- parse.py
+++ parse.py
@@ -1,23 +1,17 @@
 # -*- coding: utf8 -*-
 import Tkinter as tk
 import ttk
-import Base
 
-def parse_choix(data, param, col):
+def parse_choix(data, col, param):
     Liste = []
     tmp = []
     variable = data[0][0][col]
     for ligne in data[0] :
-        if ligne[param] == variable:
-            tmp.append(ligne[col])
+        if ligne[col] == variable:
+            tmp.append(ligne[param])
         else :
-            variable = ligne[param]
+            variable = ligne[col]
             Liste.append(tmp)
             tmp = []
-            tmp.append(ligne[col])
+            tmp.append(ligne[param])
     return Liste
-
-
-
-
-
